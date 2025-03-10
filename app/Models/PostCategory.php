a0864fb1b4396e2a3fe5100cc7790da962e10e14
Your Name <you@example.com>
2025-03-10 13:52:29 +0500

    MINOR: Add PostCategories page at admin panel

---
+++ app/Models/PostCategory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+
+use \Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+class PostCategory extends Model
+{
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->slug = Str::slug($model->name);
+        });
+    }
+}
--- database/migrations/2025_03_10_075223_create_posts_table.php
+++ database/migrations/2025_03_10_075223_create_posts_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('notifications', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('type');
-            $table->morphs('notifiable');
-            $table->text('data');
-            $table->timestamp('read_at')->nullable();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('posts');
     }
 };
